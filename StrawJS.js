d2647beca0c4ed13b7550d327a7d208056a89936
Nuggew <124299660+Nuggew@users.noreply.github.com>
2024-06-08 18:55:27 -0300

    Add files via upload

---
+++ StrawJS.js
@@ -0,0 +1,30 @@
+/** COPYRIGHT (C) 2024  **/
+/** ALL RIGHTS RESERVED **/
+
+function notify(title, body, icon, tag) {
+    if (!("Notification" in window))
+        return 'no_support';
+    else if (Notification.permission === "granted") {
+        const notification = new Notification(title, { body, icon, tag });
+        return notification;
+    } else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then((permission) => {
+            if (permission === "granted")
+                return notify(title, body, icon, tag);
+            else
+                return permission;
+        });
+    }
+}
+
+function askNotifyPermission(){
+    if (!("Notification" in window))
+        return 'no_support';
+    else{
+        Notification.requestPermission().then((permission) => {
+            return permission;
+        });
+    }
+}
+
+module.exports = {askNotifyPermission,notify};
